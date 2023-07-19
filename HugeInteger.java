ff486acf3eb25fd9b2ce8276f5f3ab903461b0f0
dawsonblack <134227563+dawsonblack@users.noreply.github.com>
2023-07-19 11:33:30 -0400

    Delete HugeInteger.java

--- HugeInteger.java
+++
@@ -1,9 +0,0 @@
-import java.util.BigInteger
-
-public class HugeInteger {
-    public BigInteger number;
-    
-    public HugeInteger(String number) {
-        this.number = new BigInteger(number);
-    }
-}
